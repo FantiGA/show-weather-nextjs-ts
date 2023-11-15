1a75598cf51339afe9eaeb607cfce81145632d3a
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-11-15 19:39:39 +0900

    Update env settings && test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import styles from './page.module.css';
 
+const weatherApiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY || process.env.WEATHER_API_KEY;
+
 export default function Home() {
   return (
-    <div className={styles.main}>Hello!, API Key: {process.env.WEATHER_API_KEY}</div>
+    <div className={styles.main}>Hello!, API Key: {weatherApiKey}</div>
   );
 }
