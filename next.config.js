1335104ac39b271991bc93e1b99ffe939848d0b8
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-11-15 20:50:36 +0900

    Env test 4

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   basePath: "/show-weather-nextjs-ts",
   publicRuntimeConfig: {
-    WEATHER_API_KEY: process.env.NEXT_PUBLIC_WEATHER_API_KEY,
+    NEXT_PUBLIC_WEATHER_API_KEY: process.env.NEXT_PUBLIC_WEATHER_API_KEY,
   },
   output: "export"
 };
