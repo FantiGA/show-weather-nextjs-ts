0578799088e3f72ebcb0a7a619fe9e10903b258e
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-11-15 19:47:25 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/show-weather-nextjs-ts",
   publicRuntimeConfig: {
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
   },
