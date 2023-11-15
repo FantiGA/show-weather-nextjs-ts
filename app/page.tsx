6fbe6ad7a0ed71d9bf43d28b4c13538f59ec6eda
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-11-15 19:57:55 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import styles from './page.module.css';
 
 const weatherApiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY || process.env.WEATHER_API_KEY;
 
 export default function Home() {
+
+  useEffect(() => {
+    console.log('NEXT_PUBLIC_WEATHER_API_KEY:', process.env.NEXT_PUBLIC_WEATHER_API_KEY);
+  }, []);
+
   return (
     <div className={styles.main}>Hello!, API Key: {weatherApiKey}</div>
   );
