bd0b5562c12ef8afa152658890f4e3fb22c89d88
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-11-15 18:59:02 +0900

    Env test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.main}>Hello!</div>
+    <div className={styles.main}>Hello!, API Key: {process.env.WEATHER_API_KEY}</div>
   );
 }
