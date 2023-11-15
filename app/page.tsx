2ecebceadc86b24bd1041152294fa6a3df1a567a
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-11-15 21:23:37 +0900

    Remove Github Pages config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import styles from './page.module.css';
 
-const weatherApiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY ?? process.env.WEATHER_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY ?? process.env.WEATHER_API_KEY;
+// const apiKey = process.env.WEATHER_API_KEY;
 
 export default function Home() {
   return (
-    <div className={styles.main}>Hello!, API Key: {weatherApiKey}</div>
+    <div className={styles.main}>Hello!, API Key: {apiKey}</div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/show-weather-nextjs-ts",
+  // basePath: "/show-weather-nextjs-ts",
   publicRuntimeConfig: {
     NEXT_PUBLIC_WEATHER_API_KEY: process.env.NEXT_PUBLIC_WEATHER_API_KEY,
   },
